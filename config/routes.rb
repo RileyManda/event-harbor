730780085275e18c39abbbb01e7ae6b98453d2a6
RileyManda <rileymanda69@gmail.com>
2023-11-19 19:13:45 +0200

    Created home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  resources :homes
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
