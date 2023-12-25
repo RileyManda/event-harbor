911f4907afec599cb6246a4b3238c07d46917b0c
RileyManda <rileymanda69@gmail.com>
2023-12-25 20:32:41 +0200

    Regenerated key

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails rake assets:clean
+bundle exec rails assets:precompile
+bundle exec rails rake assets:clean
 bundle exec rake db:migrate
