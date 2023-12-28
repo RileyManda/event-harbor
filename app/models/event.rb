0eeacc5de36bf67b627a828fcf6f50d488c2e0a4
RileyManda <rileymanda69@gmail.com>
2023-12-28 21:14:55 +0200

    Implemented input field validations to model:create event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   belongs_to :category
   has_many :participants
   attribute :visibility, default: false
-
   validates :tickets_left, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :description, length: { minimum: 150, maximum: 150 }
 end
