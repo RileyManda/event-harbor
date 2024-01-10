5dc9f4a83fab533fe9796ba89813c4e584a13d54
RileyManda <rileymanda69@gmail.com>
2024-01-10 18:13:56 +0200

    Set event default:Private

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :category
   has_many :participants
-  attribute :visibility, default: false
+  attribute :event_type, default: 'Private'
+  attribute :reservations, default: false
   validates :tickets_left, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :description, length: { maximum: 150 }
